87ccb634f3ae7c4c4570b3da11cad4fb6dbdf28a
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-31 17:12:25 -0400

    Conversion project begins.

---
+++ currency_conversions.py
@@ -0,0 +1,65 @@
+
+
+class Money:
+
+    def __init__(self, value, currency):
+        self.value = value
+        self.currency = currency
+
+    def get_value(self):
+        if self.currency == 'USD':
+            return self.value*1
+            # EUR = self.value * .898
+            # JPY = self.value * 110.713
+            # BTC = self.value * .0019
+        elif self.currency == 'JPY':
+            return self.value * .00903
+            # USD = self.value * .00903
+            # EUR = self.value * .00811
+            # BTC = self.value * .000017
+        elif self.currency == 'BTC':
+            return self.value * 529.39
+            # USD = self.value * 529.39
+            # EUR = self.value * 477.13
+            # JPY = self.value * 58806.10
+        elif self.currency == 'EUR':
+            return self.value * 1.113
+            # USD = self.value * 1.113
+            # JPY = self.value * 123.264
+            # BTC = self.value * .0021
+
+    def __eq__(self, other):
+        return self.get_value() == other.get_value()
+
+    def __ne__(self, other):
+        return self.get_value() != other.get_value()
+
+    def __le__(self, other):
+        return self.get_value() <= other.get_value()
+
+    def __lt__(self, other):
+        return self.get_value() < other.get_value()
+
+    def __ge__(self, other):
+        return self.get_value() >= other.get_value()
+
+    def __gt__(self, other):
+        return self.get_value() > other.get_value()
+
+    def __add__(self, other):
+        return self.get_value() + other.get_value()
+
+    def __sub__(self, other):
+        return self.get_value() - other.get_value()
+
+    def __mul__(self, other):
+        return self.get_value() * other.get_value()
+
+    def __mod__(self, other):
+        return self.get_value() % other.get_value()
+
+test_1 = Money(1, 'BTC')
+test_2 = Money(6, 'EUR')
+print(test_1 == test_2)
+answer = test_2 + test_1
+print(answer)
