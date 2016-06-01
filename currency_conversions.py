f2c4f73dbba5dbd040570394d4d6e26b920a2122
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-31 21:20:43 -0400

    Now converts value back to original currency

--- currency_conversions.py
+++ currency_conversions.py
@@ -6,14 +6,14 @@ class Money:
         self.value = value
         self.currency = currency
 
-    def convert_to_currency(self, other):
-        if self.currency == 'JPY':
-            if other.currency == 'USD':
-                return self.converted_value * .00903
-            elif other.currency == 'EUR':
-                return self.converted_value * .00811
-            elif other.currency == 'BTC':
-                return self.converted_value * .000017
+    # def convert_to_currency(self, other):
+    #    if self.currency == 'JPY':
+    #        if other.currency == 'USD':
+    #            return self.converted_value * .00903
+    #        elif other.currency == 'EUR':
+    #            return self.converted_value * .00811
+    #        elif other.currency == 'BTC':
+    #            return self.converted_value * .000017
 
     def get_value(self):
         if self.currency == 'USD':
@@ -56,19 +56,50 @@ class Money:
         return self.get_value() > other.get_value()
 
     def __add__(self, other):
-        return self.get_value() + self.get_value()
+        added_value = self.get_value() + self.get_value()
+        if self.currency == 'USD':
+            return added_value
+        elif self.currency == 'JPY':
+            return added_value * 110.713
+        elif self.currency == 'EUR':
+            return added_value * .898
+        elif self.currency == 'BTC':
+            return added_value * .0019
 
+    # def __sub__(self, other):
+    #    return self.get_value() - other.get_value()
     def __sub__(self, other):
-        return self.get_value() - other.get_value()
+        sub_value = self.get_value() - other.get_value()
+        if self.currency == 'USD':
+            return sub_value
+        elif self.currency == 'JPY':
+            return sub_value * 110.713
+        elif self.currency == 'EUR':
+            return sub_value * .898
+        elif self.currency == 'BTC':
+            return sub_value * .0019
 
     def __mul__(self, other):
-        return self.get_value() * other.get_value()
+        mul_value = self.get_value() * other.get_value()
+        if self.currency == 'USD':
+            return mul_value
+        elif self.currency == 'JPY':
+            return mul_value * 110.713
+        elif self.currency == 'EUR':
+            return mul_value * .898
+        elif self.currency == 'BTC':
+            return mul_value * .0019
 
     def __mod__(self, other):
-        return self.get_value() % other.get_value()
+        mod_value = self.get_value() % other.get_value()
+        if self.currency == 'USD':
+            return mod_value
+        elif self.currency == 'JPY':
+            return mod_value * 110.713
+        elif self.currency == 'EUR':
+            return mod_value * .898
+        elif self.currency == 'BTC':
+            return mod_value * .0019
 
-test_1 = Money(1, 'BTC')
-test_2 = Money(6, 'EUR')
-print(test_1 == test_2)
-print(test_2 + test_1)
-print(test_2 % test_1)
+print(Money(1, 'BTC') + Money(45, 'EUR'))
+print(Money(28, 'EUR') - Money(400, 'JPY'))
